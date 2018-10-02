a69aaf931ede94de9fe892503f07145631d4d2d9
FiammaV <ines.perdigao.nunes@gmail.com>
2018-10-02 16:56:00 +0100

    Started the project

---
+++ MyAula1/MyFortnite/FNPlayer.cs
@@ -0,0 +1,19 @@
+﻿namespace MyFortnite {
+    class FNPlayer {
+        public int ammo;
+        public float health;
+
+        public float Attack(FNPlayer enemy) {
+
+            float damage = 10;
+            enemy.TakeDamage(damage);
+            return damage;
+        }
+
+        public void TakeDamage(float damage) {
+
+            health -= damage;
+        }
+
+    }
+}
